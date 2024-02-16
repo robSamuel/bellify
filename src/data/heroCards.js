f609f20f2b184b0ea99e0c16fdf2cffd46e93173
Robert Samuel <robert_samuel97@hotmail.com>
2024-02-16 09:43:20 -0600

    Initial commit

---
+++ src/data/heroCards.js
@@ -0,0 +1,57 @@
+import LogoReact from '@/assets/images/react-card.png';
+import LogoPython from '@/assets/images/python-card.png';
+
+export const heroCards = [
+  {
+    title: 'Frontend developer',
+    technology: 'React',
+    text: 'Software Engineer with over 6 years of experience in Web development. I enjoy practicing photography.',
+    logo: LogoReact,
+    skills: [
+      {
+        id: 1,
+        text: 'JavaScript',
+      },
+      {
+        id: 2,
+        text: 'React',
+      },
+      {
+        id: 3,
+        text: 'HTML',
+      },
+      {
+        id: 4,
+        text: 'CSS',
+      },
+    ],
+  },
+  {
+    title: 'Backend developer',
+    technology: 'Python',
+    text: 'Dedicated Software Engineer with over 8 years of experience in backend development. Enthusiastic about open-source projects and contributing to Python communities.',
+    logo: LogoPython,
+    skills: [
+      {
+        id: 1,
+        text: 'Python',
+      },
+      {
+        id: 2,
+        text: 'Django',
+      },
+      {
+        id: 3,
+        text: 'Flask',
+      },
+      {
+        id: 4,
+        text: 'REST APIs',
+      },
+      {
+        id: 5,
+        text: 'SQL ',
+      },
+    ],
+  },
+]
