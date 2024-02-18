fff33c84b3b6866277dd013a2e5928af52593a9c
Robert Samuel <robert_samuel97@hotmail.com>
2024-02-18 17:04:38 -0600

    Pages - Create the pages for Products and Product Detail

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,9 @@
-import Footer from '@/components/Footer';
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <main>
-      <Footer />
+      <Link href="/products" >Products</Link>
     </main>
   );
 };
