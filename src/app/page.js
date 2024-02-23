e1e80109f15dadbdc1493dca0d57b24bc93c9e74
Josue Lopez <josue.ismael.lopez.ramirez@gmail.com>
2024-02-23 09:25:48 -0600

    added product detail section

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,15 @@
 import Banner from '@/components/Banner';
 import Footer from '@/components/Footer';
 import Header from '@/components/Header';
+import Product from '@/components/Product';
+
 export default function Home() {
   return (
     <main>
       <Banner />
       <Header />
-      <Footer />
+      <Product />
+      {/* <Footer /> */}
     </main>
   );
 };
