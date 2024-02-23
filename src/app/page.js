1e9c5a8c52b62cfddabc275ece5a985ff33239ff
Robert Samuel <robert_samuel97@hotmail.com>
2024-02-23 12:15:00 -0600

    Products List - Create the Products List component and add the lists in the index page

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,11 @@
+import ProductsList from "@/components/ProductsList";
+import { relatedProducts } from '@/data/products-list';
 
 export default function Home() {
   return (
     <main>
-      
+      <ProductsList title="Productos de la misma colección" products={relatedProducts} />
+      <ProductsList title="Productos relacionados" products={relatedProducts} />
     </main>
   );
 };
